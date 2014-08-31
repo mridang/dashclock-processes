5fb986c060efb86d8eafe8648fe7f8fd89f8f23b
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2014-08-31 11:38:34 +0300

    Made minor fixes, lowered SDK requirements and used ACRA instead of Bugsense

--- src/com/mridang/processes/ProcessesWidget.java
+++ src/com/mridang/processes/ProcessesWidget.java
@@ -2,15 +2,17 @@ package com.mridang.processes;
 
 import java.util.Random;
 
+import org.acra.ACRA;
+
 import android.app.ActivityManager;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.util.Log;
 
-import com.bugsense.trace.BugSenseHandler;
 import com.google.android.apps.dashclock.api.DashClockExtension;
 import com.google.android.apps.dashclock.api.ExtensionData;
 
@@ -26,7 +28,7 @@ public class ProcessesWidget extends DashClockExtension {
 
 		super.onCreate();
 		Log.d("ProcessesWidget", "Created");
-		BugSenseHandler.initAndStartSession(this, getString(R.string.bugsense));
+		ACRA.init(new AcraApplication(getApplicationContext()));
 
 	}
 
@@ -36,7 +38,7 @@ public class ProcessesWidget extends DashClockExtension {
 	 * (int)
 	 */
 	@Override
-	protected void onUpdateData(int arg0) {
+	protected void onUpdateData(int intReason) {
 
 		Log.d("ProcessesWidget", "Getting the status of the processes");
 		ExtensionData edtInformation = new ExtensionData();
@@ -44,32 +46,26 @@ public class ProcessesWidget extends DashClockExtension {
 
 		try {
 
-			ActivityManager mgrActivity = (ActivityManager) this.getSystemService( ACTIVITY_SERVICE );
+			ActivityManager mgrActivity = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
 
-			edtInformation.clickIntent(new Intent(Intent.ACTION_VIEW).setClassName("com.android.settings", "com.android.settings.RunningServices"));
-			
-			Log.d("ProcessesWidget", "Calculating the number of recent activities");
-			Integer intActivities =  mgrActivity.getRecentTasks(Integer.MAX_VALUE, ActivityManager.RECENT_IGNORE_UNAVAILABLE).size() - 1;
-			Log.d("ProcessesWidget", intActivities + " recent activities");
+			Log.d("ProcessesWidget", "Calculating the number of tasks and swrvices");
+			Integer intServices = mgrActivity.getRunningServices(Integer.MAX_VALUE).size();
+			Integer intRecents = mgrActivity.getRecentTasks(Integer.MAX_VALUE, 2).size() - 1;
+			Integer intProcess = mgrActivity.getRunningAppProcesses().size();
+			String strServices = getResources().getQuantityString(R.plurals.service, intServices, intServices);
+			String strRecents = getResources().getQuantityString(R.plurals.activity, intRecents, intRecents);
 
-			edtInformation.status(Integer.toString(intActivities));
-			if (intActivities > 0) 
-				edtInformation.expandedTitle(getResources().getQuantityString(R.plurals.activity, intActivities, intActivities));
-			else
-				edtInformation.expandedTitle(getString(R.string.no_activities));
+			Log.d("ProcessesWidget", String.format("%d activities and %s services runnung", intRecents, intServices));
+			Log.d("ProcessesWidget", intProcess + " running applications");
 
-			Log.d("ProcessesWidget", "Calculating the number of running applications");
-			Integer intApplications = mgrActivity.getRunningAppProcesses().size();
-			Log.d("ProcessesWidget", intApplications + " running applications");
-
-			if (intApplications > 0) 
-				edtInformation.expandedBody(getResources().getQuantityString(R.plurals.process, intApplications, intApplications));
-			else
-				edtInformation.expandedTitle(getString(R.string.no_processes));			
-
+			edtInformation.clickIntent(new Intent(Intent.ACTION_VIEW).setClassName("com.android.settings",
+					"com.android.settings.RunningServices"));
+			edtInformation.status(Integer.toString(intRecents + intServices));
+			edtInformation.expandedTitle(getString(R.string.and, strRecents, strServices));
+			edtInformation.expandedBody(getResources().getQuantityString(R.plurals.process, intProcess, intProcess));
 			edtInformation.visible(true);
 
-			if (new Random().nextInt(5) == 0) {
+			if (new Random().nextInt(5) == 0 && !(0 != (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE))) {
 
 				PackageManager mgrPackages = getApplicationContext().getPackageManager();
 
@@ -80,36 +76,38 @@ public class ProcessesWidget extends DashClockExtension {
 				} catch (NameNotFoundException e) {
 
 					Integer intExtensions = 0;
-				    Intent ittFilter = new Intent("com.google.android.apps.dashclock.Extension");
-				    String strPackage;
+					Intent ittFilter = new Intent("com.google.android.apps.dashclock.Extension");
+					String strPackage;
 
-				    for (ResolveInfo info : mgrPackages.queryIntentServices(ittFilter, 0)) {
+					for (ResolveInfo info : mgrPackages.queryIntentServices(ittFilter, 0)) {
 
-				    	strPackage = info.serviceInfo.applicationInfo.packageName;
-						intExtensions = intExtensions + (strPackage.startsWith("com.mridang.") ? 1 : 0); 
+						strPackage = info.serviceInfo.applicationInfo.packageName;
+						intExtensions = intExtensions + (strPackage.startsWith("com.mridang.") ? 1 : 0);
 
 					}
 
 					if (intExtensions > 1) {
 
 						edtInformation.visible(true);
-						edtInformation.clickIntent(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("market://details?id=com.mridang.donate")));
+						edtInformation.clickIntent(new Intent(Intent.ACTION_VIEW).setData(Uri
+								.parse("market://details?id=com.mridang.donate")));
 						edtInformation.expandedTitle("Please consider a one time purchase to unlock.");
-						edtInformation.expandedBody("Thank you for using " + intExtensions + " extensions of mine. Click this to make a one-time purchase or use just one extension to make this disappear.");
+						edtInformation
+								.expandedBody("Thank you for using "
+										+ intExtensions
+										+ " extensions of mine. Click this to make a one-time purchase or use just one extension to make this disappear.");
 						setUpdateWhenScreenOn(true);
 
 					}
 
 				}
 
-			} else {
-				setUpdateWhenScreenOn(true);
 			}
 
 		} catch (Exception e) {
 			edtInformation.visible(false);
 			Log.e("ProcessesWidget", "Encountered an error", e);
-			BugSenseHandler.sendException(e);
+			ACRA.getErrorReporter().handleSilentException(e);
 		}
 
 		edtInformation.icon(R.drawable.ic_dashclock);
@@ -125,7 +123,6 @@ public class ProcessesWidget extends DashClockExtension {
 
 		super.onDestroy();
 		Log.d("ProcessesWidget", "Destroyed");
-		BugSenseHandler.closeSession(this);
 
 	}
 
